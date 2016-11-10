bcf67f142d70da7d0070ec1100b44b5ccc158226
Robert Woods <robertwoods2013@gmail.com>
2016-11-10 16:48:50 -0500

    init

---
+++ app/src/main/java/edu/temple/browser/MainActivity.java
@@ -0,0 +1,105 @@
+package edu.temple.browser;
+
+import android.app.Activity;
+import android.app.FragmentManager;
+import android.net.Uri;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.MenuItem;
+import android.widget.Toolbar;
+import android.webkit.WebView;
+import android.widget.Toast;
+
+import java.util.ArrayList;
+
+public class MainActivity extends Activity implements NavigationFragment.OnFragmentInteractionListener{
+
+    ArrayList<WebViewFragment> webFragments = new ArrayList<WebViewFragment>(1);
+    int currentWebFragment = 0;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
+        setActionBar(toolbar);
+
+        FragmentManager fm = getFragmentManager();
+        WebViewFragment webViewFrag = new WebViewFragment();
+        Uri data = getIntent().getData();
+        Bundle bundle = new Bundle();
+        bundle.putString(webViewFrag.URL_FETCHER,
+                data != null ? data.toString() : "https://google.com");
+        webFragments.add(webViewFrag);
+        webViewFrag.setArguments(bundle);
+        NavigationFragment navFrag = new NavigationFragment();
+        fm.beginTransaction().replace(R.id.browserFrame, webViewFrag)
+                .replace(R.id.navigationFrame, navFrag).commit();
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item){
+        switch(item.getItemId()){
+            case R.id.newButton:
+                onNew();
+                break;
+            case R.id.nextButton:
+                onNext();
+                break;
+            case R.id.prevButton:
+                onPrev();
+                break;
+            default:
+                return super.onOptionsItemSelected(item);
+        }
+        return true;
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu){
+        MenuInflater inf = getMenuInflater();
+        inf.inflate(R.menu.appbar, menu);
+        return true;
+    }
+
+    public void onNext() {
+        try {
+            WebViewFragment webViewFrag = webFragments.get(currentWebFragment+1);
+            currentWebFragment++;
+            getFragmentManager().beginTransaction().hide(webFragments.get(currentWebFragment-1))
+                    .show(webViewFrag)
+                    .commit();
+        } catch(IndexOutOfBoundsException e) {
+            Toast.makeText(this, "No more pages", Toast.LENGTH_SHORT).show();
+        }
+    }
+
+    public void onPrev() {
+        try {
+            WebViewFragment webViewFrag = webFragments.get(currentWebFragment-1);
+            currentWebFragment--;
+            getFragmentManager().beginTransaction().hide(webFragments.get(currentWebFragment+1))
+                    .show(webViewFrag)
+                    .commit();
+        } catch(IndexOutOfBoundsException e) {
+            Toast.makeText(this, "No more pages", Toast.LENGTH_SHORT).show();
+        }
+    }
+
+    public void onNew() {
+        WebViewFragment webViewFrag = new WebViewFragment();
+        webFragments.add(webViewFrag);
+        int oldWebFragment = currentWebFragment;
+        currentWebFragment = webFragments.size() - 1;
+        getFragmentManager().beginTransaction().hide(webFragments.get(oldWebFragment)).
+                add(R.id.browserFrame, webViewFrag)
+                .commit();
+    }
+
+    @Override
+    public void onTextEntered(String url) {
+        webFragments.get(currentWebFragment).navigateToUrl(url);
+    }
+}
---
+++ app/src/main/java/edu/temple/browser/WebViewFragment.java
@@ -0,0 +1,52 @@
+package edu.temple.browser;
+
+
+import android.content.Context;
+import android.os.Bundle;
+import android.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.webkit.WebView;
+import android.webkit.WebViewClient;
+import android.widget.Button;
+import android.widget.Toast;
+
+
+/**
+ * A simple {@link Fragment} subclass.
+ */
+public class WebViewFragment extends Fragment {
+
+    public final String URL_FETCHER = "ejfwoijoiejfaeoiwjf";
+
+    public WebViewFragment() {
+        // Required empty public constructor
+    }
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        // Inflate the layout for this fragment
+        View v =  inflater.inflate(R.layout.fragment_web_view, container, false);
+        if(getArguments() != null && getArguments().getString(URL_FETCHER)!=null) {
+            String url = getArguments().getString(URL_FETCHER);
+            ((WebView) v.findViewById(R.id.mainWebView)).setWebViewClient(new WebViewClient());
+            if(!url.toLowerCase().contains("http://")&&!url.toLowerCase().contains("https://"))
+                url = "https://" + url ;
+            ((WebView) v.findViewById(R.id.mainWebView)).loadUrl(url);
+        } else {
+            ((WebView) v.findViewById(R.id.mainWebView)).loadUrl("https://www.google.com");
+        }
+        return v;
+    }
+
+    public void navigateToUrl(String url){
+        if(!url.contains("http://")&&!url.contains("https://"))
+            url = "https://" + url ;
+        WebView wv = (WebView) getView().findViewById(R.id.mainWebView);
+        wv.setWebViewClient(new WebViewClient());
+        wv.loadUrl(url);
+    }
+
+}
